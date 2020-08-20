7833524529bf6bec679b67cf55e3209cb4f89aed
Steve Jefferson <steve@stevejefferson.co.uk>
2020-08-20 14:42:34 +0100

    Added licencing to all sources.

--- accessor/trac/path.go
+++ accessor/trac/path.go
@@ -1,3 +1,6 @@
+// Copyright 2020 Steve Jefferson. All rights reserved.
+// Use of this source code is governed by a GPL-style
+// license that can be found in the LICENSE file.
 package trac
 
 import "path/filepath"
